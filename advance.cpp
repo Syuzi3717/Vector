60f54d817d37f26d507a68082188bafc83877b26
Syuzi3717 <84679479+Syuzi3717@users.noreply.github.com>
2022-05-17 18:58:29 +0400

    Update advance.cpp

--- advance.cpp
+++ advance.cpp
@@ -3,29 +3,25 @@
 #include <vector>
 
 namespace my_advance{
-
-    template<typename It>
-    constexpr void _advance(It& it, typename std::iterator_traits<It>::difference_type n, std::forward_iterator_tag )
+    template<typename It, typename D >
+    constexpr void m_advance(It& it, D n)
     {
-        std::cout<<"third"<<std::endl;
+        _advance( it, typename std::iterator_traits<It>::difference_type(n), typename std::iterator_traits<It>::iterator_category() );
+    } 
+    
+    template<typename It>
+    constexpr void _advance(It& it, typename std::iterator_traits<It>::difference_type n, std::forward_iterator_tag)
+    {
         while (n > 0) 
         {
             --n;
             ++it;
         }
     }
-
-    template <typename It>
-    constexpr void _advance(It& it, typename std::iterator_traits<It>::difference_type n, std::random_access_iterator_tag )
-    {
-        std::cout<<"second"<<std::endl;
-        it+=n;
-    }
-
+    
     template<typename It>
-    constexpr void _advance(It& it, typename std::iterator_traits<It>::difference_type n, std::bidirectional_iterator_tag )
+    constexpr void _advance(It& it, typename std::iterator_traits<It>::difference_type n, std::bidirectional_iterator_tag)
     {
-        std::cout<<"first"<<std::endl;
         while (n > 0) 
         {
             --n;
@@ -37,19 +33,19 @@ namespace my_advance{
             --it;
         }
     }
-}
 
-template<typename It, typename D >
-constexpr void _advance(It& it, D n){
-    std::cout<<"final"<<std::endl;
-    my_advance::_advance(it, typename std::iterator_traits<It>::difference_type(n), typename std::iterator_traits<It>::iterator_category() );
-} 
+    template <typename It>
+    constexpr void _advance(It& it, typename std::iterator_traits<It>::difference_type n, std::random_access_iterator_tag)
+    {
+        it += n;
+    }
+}
 
 int main() 
 {
     std::vector<int> v{ 1, 2, 3, 4, 5, 6 };
     auto ptr = v.begin();
-    ::_advance(ptr, 4);
+    my_advance::m_advance(ptr, 4);
     std::cout << *ptr << std::endl;
 
 }
